0f4cea8df2957316438eb57cf19ed38ba75e8662
Pepel777 <pepel77777@gmail.com>
2019-08-07 22:28:28 +0300

    Initial commit

---
+++ src/Grebenchikov3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Grebenchikov3 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введи любое целое число:");
+        int i = s.nextInt();
+        if (i>7){
+            System.out.println("Привет");
+        }
+    }
+}
